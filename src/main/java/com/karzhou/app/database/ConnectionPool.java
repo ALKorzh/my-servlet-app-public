7c3cd8e8565c2d209e2760d384a4add45cfbd38c
Aliaksei Karzhou <alexkorzhwork@gmail.com>
2025-05-20 01:13:20 +0300

    Refactor directories

--- src/main/java/com/karzhou/app/database/ConnectionPool.java
+++
@@ -1,9 +0,0 @@
-package com.karzhou.app.database;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-
-public interface ConnectionPool {
-    Connection getConnection() throws SQLException;
-    void releaseConnection(Connection connection);
-}
--- src/main/java/com/karzhou/database/ConnectionPool.java
+++
@@ -1,9 +0,0 @@
-package com.karzhou.database;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-
-public interface ConnectionPool {
-    Connection getConnection() throws SQLException;
-    void releaseConnection(Connection connection);
-}
